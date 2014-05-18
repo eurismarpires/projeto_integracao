0b2717adcc515df29b13e271609d2b460e03376a
Eurismar Pires Borges <eurismarpires@gmail.com>
2014-05-18 02:42:45 -0300

    Implementado os métodos de registrar no GCM

--- ClientGCMIntegracao/src/com/eurismar/clientgcmintegracao/RegistrarActivity.java
+++ ClientGCMIntegracao/src/com/eurismar/clientgcmintegracao/RegistrarActivity.java
@@ -1,24 +1,47 @@
 package com.eurismar.clientgcmintegracao;
 
+import java.io.IOException;
+
 import com.example.clientgcmintegracao.R;
 import com.example.clientgcmintegracao.R.id;
 import com.example.clientgcmintegracao.R.layout;
 import com.example.clientgcmintegracao.R.menu;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 import android.app.Activity;
 import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.os.Build;
 
 public class RegistrarActivity extends Activity {
+	public static final String EXTRA_MESSAGE = "message";
+	public static final String PROPERTY_REG_ID = "registration_id";
+	private static final String PROPERTY_APP_VERSION = "1";
+	private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
 
+	private static final String TAG = "Eurismar";
+	String SENDER_ID = "494593702108";
+	Context context;
+	GoogleCloudMessaging gcm;
+	String regid;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -51,6 +74,10 @@ public class RegistrarActivity extends Activity {
 			Intent intent = new Intent(this,MainActivity.class);
 			startActivity(intent);			
 			return true;
+		}
+		if(id == R.id.cancelarRegistro){
+			desRegistrar();
+			Mensagem("Atenção", "Registro Cancelado!");
 		}		
 		return super.onOptionsItemSelected(item);
 	}
@@ -71,5 +98,164 @@ public class RegistrarActivity extends Activity {
 			return rootView;
 		}
 	}
+	public void onClick(View v) {
+		// Mensagem("Olá", "Estou testando o click");
+		EditText edt = (EditText) findViewById(R.id.edtReg);
+		if (v.getId() == R.id.btnRegistrar) {
+			context = getApplicationContext();
+			Log.i("Onclick", "CHEGOU AKI.........................1");
+			if (checkPlayServices()) {
+				Log.i("Onclick", "CHEGOU AKI.............................2");
+				gcm = GoogleCloudMessaging.getInstance(this);
+				Log.i("Onclick", "CHEGOU AKI.............................3");
+				regid = getRegistrationId(context);
+				Log.i("Onclick", "CHEGOU AKI.............................4");
+				if (regid.isEmpty()) {
+					Log.i("Onclick", "CHEGOU AKI.............................5");
+					registerInBackground();
+					edt.setText(regid);
+				}
+			} else {
+				Log.i("Onclick", "CHEGOU AKI.............................6");
+				Toast t = Toast.makeText(getApplicationContext(),
+						"Serviço Google Play não encontrado!",
+						Toast.LENGTH_LONG);
+				t.show();
+			}
+		}
+		// / else if (v.getId() == R.id.btnDesregistrar) {
+		// Mensagem("Atenção", "Vou desregistrar");
+		// }
+		
+		edt.setText(regid);
+	}	
+	private void desRegistrar() {
 
+		if (gcm == null) {
+			gcm = GoogleCloudMessaging.getInstance(context);
+		}
+		try {
+			Log.e("EURISMAR", "VOU DESREGISTRAR");
+			gcm.unregister();
+			
+			
+			Log.e("EURISMAR", "DESREGISTROU");
+			
+			final SharedPreferences prefs = getGCMPreferences(context);						
+			SharedPreferences.Editor editor = prefs.edit();
+			editor.clear();
+			editor.commit();			
+			
+			Log.e("EURISMAR", "APAGOU O CACHE");					
+			
+		} catch (IOException e) {
+
+			Mensagem("Erro", e.getMessage());
+		}
+	}
+	public void Mensagem(String titulo, String texto) {
+		AlertDialog.Builder mensagem = new AlertDialog.Builder(this);
+		mensagem.setTitle(titulo);
+		mensagem.setMessage(texto);
+		mensagem.setNeutralButton("OK", null);
+		mensagem.show();
+	}
+	private void registerInBackground() {
+		new AsyncTask<Void, Void, String>() {
+			@Override
+			protected String doInBackground(Void... params) {
+				String msg = "";
+				try {
+					if (gcm == null) {
+						gcm = GoogleCloudMessaging.getInstance(context);
+					}
+
+					regid = gcm.register(SENDER_ID);
+
+					Log.d("RegisterActivity", "registerInBackground - regId: "
+							+ regid);
+					msg = "Device registered, registration ID=" + regid;
+
+					storeRegistrationId(context, regid);
+				} catch (IOException ex) {
+					msg = "Error :" + ex.getMessage();
+					Log.d("RegisterActivity", "Error: " + msg);
+				}
+				Log.d("RegisterActivity", "AsyncTask completed: " + msg);
+				return msg;
+			}
+
+			@Override
+			protected void onPostExecute(String msg) {
+				Toast.makeText(getApplicationContext(),
+						"Registered with GCM Server." + regid,
+						Toast.LENGTH_LONG).show();
+
+			}
+		}.execute(null, null, null);
+	}
+
+	private void storeRegistrationId(Context context, String regId) {
+		final SharedPreferences prefs = getGCMPreferences(context);
+		int appVersion = getAppVersion(context);
+		Log.i(TAG, "Saving regId on app version " + appVersion);
+		SharedPreferences.Editor editor = prefs.edit();
+		editor.putString(PROPERTY_REG_ID, regId);
+		editor.putInt(PROPERTY_APP_VERSION, appVersion);
+		editor.commit();
+	}
+
+	private static int getAppVersion(Context context) {
+		try {
+			PackageInfo packageInfo = context.getPackageManager()
+					.getPackageInfo(context.getPackageName(), 0);
+			return packageInfo.versionCode;
+		} catch (NameNotFoundException e) {
+			// should never happen
+			throw new RuntimeException("Could not get package name: " + e);
+		}
+	}
+
+	private SharedPreferences getGCMPreferences(Context context) {
+		// This sample app persists the registration ID in shared preferences,
+		// but
+		// how you store the regID in your app is up to you.
+		return getSharedPreferences(MainActivity.class.getSimpleName(),
+				Context.MODE_PRIVATE);
+	}
+	
+	private String getRegistrationId(Context context) {
+		final SharedPreferences prefs = getGCMPreferences(context);
+		String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+		if (registrationId.isEmpty()) {
+			Log.i(TAG, "Registration not found.");
+			return "";
+		}
+		// Check if app was updated; if so, it must clear the registration ID
+		// since the existing regID is not guaranteed to work with the new
+		// app version.
+		int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION,
+				Integer.MIN_VALUE);
+		int currentVersion = getAppVersion(context);
+		if (registeredVersion != currentVersion) {
+			Log.i(TAG, "App version changed.");
+			return "";
+		}
+		return registrationId;
+	}
+	private boolean checkPlayServices() {
+		int resultCode = GooglePlayServicesUtil
+				.isGooglePlayServicesAvailable(this);
+		if (resultCode != ConnectionResult.SUCCESS) {
+			if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+				GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+						PLAY_SERVICES_RESOLUTION_REQUEST).show();
+			} else {
+				Log.i(TAG, "This device is not supported.");
+				finish();
+			}
+			return false;
+		}
+		return true;
+	}
 }
