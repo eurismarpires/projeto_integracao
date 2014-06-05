ad05774ae3fa800605d4d07dee9deebf15ca7d5c
Eurismar Pires Borges <eurismarpires@gmail.com>
2014-06-04 23:17:55 -0300

    Coloquei para funcionar o recebimento de mensagens

--- ClientGCMIntegracao/src/com/eurismar/clientgcmintegracao/GcmBroadcastReceiver.java
+++ ClientGCMIntegracao/src/com/eurismar/clientgcmintegracao/GcmBroadcastReceiver.java
@@ -21,15 +21,14 @@ import android.util.Log;
 
 public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-    	// Explicitly specify that GcmMessageHandler will handle the intent.
-        ComponentName comp = new ComponentName(context.getPackageName(),
-        		GcmIntentService.class.getName());
-        
-        // Start the service, keeping the device awake while it is launching.
-        startWakefulService(context, (intent.setComponent(comp)));
-        setResultCode(Activity.RESULT_OK);
-    }
+	  @Override
+	    public void onReceive(Context context, Intent intent) {
+	        // Explicitly specify that GcmIntentService will handle the intent.
+	        ComponentName comp = new ComponentName(context.getPackageName(),
+	                GcmIntentService.class.getName());
+	        // Start the service, keeping the device awake while it is launching.
+	        startWakefulService(context, (intent.setComponent(comp)));
+	        setResultCode(Activity.RESULT_OK);
+	    }
 }
 
--- ClientGCMIntegracao/src/com/eurismar/clientgcmintegracao/GcmIntentService.java
+++ ClientGCMIntegracao/src/com/eurismar/clientgcmintegracao/GcmIntentService.java
@@ -1,5 +1,5 @@
 package com.eurismar.clientgcmintegracao;
-//package com.example.gcmexample;
+
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
@@ -15,53 +15,80 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
-/**
- * This {@code IntentService} does the actual handling of the GCM message.
- * {@code GcmBroadcastReceiver} (a {@code WakefulBroadcastReceiver}) holds a
- * partial wake lock for this service while the service does its work. When the
- * service is finished, it calls {@code completeWakefulIntent()} to release the
- * wake lock.
- */
 public class GcmIntentService extends IntentService {
-	String mes;
-    private Handler handler;
-	public GcmIntentService() {
-		super("GcmIntentService");
-	}
+    public static final int NOTIFICATION_ID = 1;
+    private NotificationManager mNotificationManager;
+    NotificationCompat.Builder builder;
+    String TAG = "Teste GCM";
+    public GcmIntentService() {
+        super("GcmIntentService");
+    }
 
-	@Override
-	public void onCreate() {
-		// TODO Auto-generated method stub
-		super.onCreate();
-		handler = new Handler();
-	}
-	@Override
-	protected void onHandleIntent(Intent intent) {
-       Bundle extras = intent.getExtras();
-       
-       GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
-       // The getMessageType() intent parameter must be the intent you received
-       // in your BroadcastReceiver.
-       String messageType = gcm.getMessageType(intent);
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Bundle extras = intent.getExtras();
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+        // The getMessageType() intent parameter must be the intent you received
+        // in your BroadcastReceiver.
+        String messageType = gcm.getMessageType(intent);
 
-      mes = extras.getString("title");
-      showToast();
-      Log.i("GCM", "Received : (" +messageType+")  "+extras.getString("title"));
-      
-             
-       
-       GcmBroadcastReceiver.completeWakefulIntent(intent);
+        if (!extras.isEmpty()) {  // has effect of unparcelling Bundle
+            /*
+             * Filter messages based on message type. Since it is likely that GCM
+             * will be extended in the future with new message types, just ignore
+             * any message types you're not interested in, or that you don't
+             * recognize.
+             */
+            if (GoogleCloudMessaging.
+                    MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
+                sendNotification("Send error: " + extras.toString());
+            } else if (GoogleCloudMessaging.
+                    MESSAGE_TYPE_DELETED.equals(messageType)) {
+                sendNotification("Deleted messages on server: " +
+                        extras.toString());
+            // If it's a regular GCM message, do some work.
+            } else if (GoogleCloudMessaging.
+                    MESSAGE_TYPE_MESSAGE.equals(messageType)) {
+                // This loop represents the service doing some work.
+                for (int i=0; i<5; i++) {
+                    Log.i(TAG, "Working... " + (i+1)
+                            + "/5 @ " + SystemClock.elapsedRealtime());
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                    }
+                }
+                Log.i(TAG, "Completed work @ " + SystemClock.elapsedRealtime());
+                // Post notification of received message.
+                sendNotification("Received: " + extras.toString());
+                Log.i(TAG, "Received: " + extras.toString());
+            }
+        }
+        // Release the wake lock provided by the WakefulBroadcastReceiver.
+        GcmBroadcastReceiver.completeWakefulIntent(intent);
+    }
 
-	}
+    // Put the message into a notification and post it.
+    // This is just one simple example of what you might choose to do with
+    // a GCM message.
+    private void sendNotification(String msg) {
+        mNotificationManager = (NotificationManager)
+                this.getSystemService(Context.NOTIFICATION_SERVICE);
 
-	public void showToast(){
-		handler.post(new Runnable() {
-		    public void run() {
-		        Toast.makeText(getApplicationContext(),mes , Toast.LENGTH_LONG).show();
-		    }
-		 });
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, MainActivity.class), 0);
 
-	}
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+        .setSmallIcon(R.drawable.ic_launcher)
+        .setContentTitle("GCM Notification")
+        .setStyle(new NotificationCompat.BigTextStyle()
+        .bigText(msg))
+        .setContentText(msg);
+
+        mBuilder.setContentIntent(contentIntent);
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+    }
 
 }
 
