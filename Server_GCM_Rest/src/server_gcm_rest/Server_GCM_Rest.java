1da7c28fb15c0e287d8b2845d8d5fb0553f3334c
Eurismar Pires Borges <eurismarpires@gmail.com>
2014-06-19 23:47:04 -0300

    fixes eurismarpires/projeto_integracao#4

--- Server_GCM_Rest/src/server_gcm_rest/Server_GCM_Rest.java
+++ Server_GCM_Rest/src/server_gcm_rest/Server_GCM_Rest.java
@@ -1,11 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package server_gcm_rest;
 
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -14,56 +15,158 @@ import java.io.Writer;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Date;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  *
- * @author Eurismar
+ * @author Eurismar Pires Borges
  */
-public class Server_GCM_Rest {
+public class Server_GCM_Rest extends JFrame {
 
-    private static final String ID_DISPOSITIVO_GCM = "APA91bGVd_Ztbihd2iNnC9drgtS-PslKKML-x1oDRJiEIDQ95UeSMid9CYMaxV7mDfSeEpqfwsAs-LoOxK-shRzoZcnq7Sw_Rf1pHJC_x2rSrJp-VFg6c_CgTNwf6A1A9Sk2BtfHnZf5mEtbhU7Hobu3-hoIGMyyo18aS7E7_UuB4glLbeXdZsA";
-// Variável com a chave obtida em API ACCESS no Google APIs
     private static final String API_KEY = "AIzaSyAuMLK2n1TQto8xmnSgMtX1ldBvGjdC4X0";
+    JButton btnEnviar = new JButton("Enviar mensagem");
+    JTextField tfDiretorio;
+    JLabel lblNomeDiretorio = new JLabel("Informe o caminho de onde está o(s) arquivo(s) gerado(s) no emulador.\n Exemplo C:\\teste\\gcm_config_seuemail@gmail.com.txt");
+    JLabel lblAssunto = new JLabel("Digite o Assunto da Mensagem");
+    JLabel lblMensagem = new JLabel("Digite a Mensagem");
+    JLabel lblAutor = new JLabel("Autor");
+    
+    JTextField tfAssunto = new JTextField("");
+    JTextField tfAutor = new JTextField("");    
+    JTextField tfMensagem = new JTextField("");
+    
+    FlowLayout layout;    
+    Container container;
+    JPanel painelPrincipal = painelPrincipal = new JPanel();
+    
+    public void adicionaComponentes(){
+        painelPrincipal.add(lblNomeDiretorio);
+        painelPrincipal.add(tfDiretorio);  
+        painelPrincipal.add(lblAutor);
+        painelPrincipal.add(tfAutor);        
+        painelPrincipal.add(lblAssunto);
+        painelPrincipal.add(tfAssunto);                
+        painelPrincipal.add(lblMensagem);
+        painelPrincipal.add(tfMensagem);        
+        painelPrincipal.add(btnEnviar);   
+    };
+    public Server_GCM_Rest() {        
+        painelPrincipal.setLayout(new BoxLayout(painelPrincipal, BoxLayout.Y_AXIS));        
+        layout = new FlowLayout();
+        container = getContentPane();                
+        setLayout(layout);                     
+        setLocation(50, 50);
+        setTitle("Aplicativo para envio de mensagens via GCM");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        tfDiretorio = new JTextField();
+        add(painelPrincipal);
+        adicionaComponentes();
+        btnEnviar.addActionListener(new ActionListener() {
 
-    public static void main(String[] args) throws MalformedURLException, IOException {
-        String request = "https://android.googleapis.com/gcm/send";
-        URL url = new URL(request);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("POST");
-        conn.setDoOutput(true);
-        conn.setDoInput(true);
-        conn.setUseCaches(false);
-        conn.setAllowUserInteraction(false);
-        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-        conn.setRequestProperty("Authorization", "key=AIzaSyAuMLK2n1TQto8xmnSgMtX1ldBvGjdC4X0");
-        OutputStream out = conn.getOutputStream();
-
-        String dados = "data.nome=Eurismar Pires Borges&data.mensagem=teste mensagem&data.hoje="+new Date();
-        
-        String registration_id = "registration_id="+ID_DISPOSITIVO_GCM;              
-        String body = dados + "&" + registration_id;
-        System.out.println(body);
-        Writer writer = new OutputStreamWriter(out, "UTF-8");
-        writer.write(body);
-
-        writer.close();
-        out.close();
-
-        if (conn.getResponseCode() != 200) {
-            System.out.println(conn.getResponseCode() + "-" + conn.getResponseMessage());
-        }
-        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = rd.readLine()) != null) {
-            sb.append(line);
-        }
-        rd.close();
-
-        conn.disconnect();
-
-        System.out.println(sb.toString());
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               
+                 String id = lerTxt();
+                 System.out.println("tripa|" + id + "|");
+                try {                
+                    enviarMensagemGCM(id);
+                } catch (IOException ex) {                    
+                     JOptionPane.showMessageDialog(null, "Erro:" + ex.getMessage());
+                }
+            }
+        });
     }
 
+    public static void main(String[] args) throws MalformedURLException, IOException {
+        Server_GCM_Rest frame = new Server_GCM_Rest();
+        frame.setVisible(true);
+        frame.setSize(800, 400);
+    }
+
+    private void enviarMensagemGCM(String idDispositivoGCM) throws IOException {        
+            String request = "https://android.googleapis.com/gcm/send";
+            URL url = new URL(request);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setUseCaches(false);
+            conn.setAllowUserInteraction(false);
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("Authorization", "key=AIzaSyAuMLK2n1TQto8xmnSgMtX1ldBvGjdC4X0");
+            OutputStream out = conn.getOutputStream();
+                      
+            String assunto = tfAssunto.getText();
+            assunto = tfAssunto.getText();
+            
+            String autor = tfAutor.getText();            
+            String mensagem = tfMensagem.getText();
+
+            String dados = "data.assunto=" + assunto + "&data.mensagem=" + mensagem + "&data.autor=" + autor;
+
+            String registration_id = "registration_id=" + idDispositivoGCM;
+            String body = dados + "&" + registration_id;
+            System.out.println(body);
+            Writer writer = new OutputStreamWriter(out, "UTF-8");
+            writer.write(body);
+
+            writer.close();
+            out.close();
+
+            if (conn.getResponseCode() != 200) {
+                System.out.println(conn.getResponseCode() + "-" + conn.getResponseMessage());
+            }
+            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                sb.append(line);
+            }
+            rd.close();
+
+            conn.disconnect();
+            JOptionPane.showMessageDialog(null, sb.toString(), "Dados enviados", JOptionPane.INFORMATION_MESSAGE);
+        
+    }
+
+    public String lerTxt() {                
+        String nomeArq = tfDiretorio.getText();
+        String linha = "";
+        File arq = new File(nomeArq);
+
+        if (arq.exists()) {
+            try {
+                FileReader reader = new FileReader(nomeArq);
+                BufferedReader leitor = new BufferedReader(reader);
+                linha = leitor.readLine();
+            } catch (Exception erro) {
+            }
+        } else {
+           JOptionPane.showMessageDialog(null,"Arquivo não encontrado" , "Dados enviados", JOptionPane.ERROR_MESSAGE);
+        }
+        return linha;
+
+    }
+
+    public static void lerDiretorio(String dir, String usuario) {
+        File arquivos[];
+        File diretorio = new File(dir);
+        arquivos = diretorio.listFiles();
+        for (int i = 0; i < arquivos.length; i++) {
+            String arquivo = arquivos[i].toString();
+            if (arquivo.contains("gcm_config")) {
+                if (arquivo.contains(usuario)) {
+                    System.out.println(arquivo);
+                } else {
+                    System.out.println(arquivo);
+                }
+            }
+        }
+    }
 }
